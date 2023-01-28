7e47c90c73da603ffe04af8d95ff378ecadcddce
William Koerich <williamkoerich17@gmail.com>
2023-01-28 14:18:26 -0300

    feat: created footer component

---
+++ test-mks/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FooterStyled } from "./footerStyled";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <FooterStyled>
+        <p>MKS sistemas © Todos os direitos reservados</p>
+      </FooterStyled>
+    )
+  }
+}
+
+export default Footer;
---
+++ test-mks/src/components/footerStyled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const FooterStyled = styled.footer`
+  margin-top: 150px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 34px;
+  background: #eeeeee;
+  & > p {
+    font-family: "Montserrat";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 15px;
+
+    color: #000000;
+  }
+`;
