80d0ef7424694c1f254b9cf652958b75e07b4eb8
William Koerich <williamkoerich17@gmail.com>
2023-01-30 20:49:35 -0300

    fds

--- test-mks/src/redux/reducers/index.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import products from "./products";
-import cart from "./cart";
-
-const rootReducer = combineReducers({ products, cart });
-
-export default rootReducer;
